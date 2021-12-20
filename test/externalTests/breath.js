34ad48df504c2d62a02b3fa95ebd02279d657b6f
Abdur Rahaman Asim <asimtipsbangla@gmail.com>
2021-12-20 13:36:43 +0600

    Update

--- test/externalTests/breath.js
+++
@@ -1,7 +0,0 @@
-const assert = require('assert')
-
-module.exports = () => async (bot) => {
-  await new Promise((resolve, reject) => setTimeout(resolve, 5000))
-  // TODO: ** Fix in 1.17
-  if (bot.oxygenLevel) assert.strictEqual(bot.oxygenLevel, 20, 'Wrong oxygen level')
-}
